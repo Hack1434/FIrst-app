5f4b2ff603a5e29aecc823f8bc3cd420b8139913
Hack1434 <uckbx001@gmail.com>
2023-12-26 17:39:27 +0530

    submit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Profile from './Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Profile />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Profile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+    <h3>Hi every this is my first react app!!</h3>
+    <h4>These are my skills</h4>
+    <ul className ="list">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React js</li>
+    </ul>
+  </div>
+  )
+}
+
+export default Profile
